583918ab6f0a8aaa8baa7d39eb44bfccd5b8fbee
Steven Jenkins <j.s.jenkins@jpl.nasa.gov>
2018-02-28 15:47:32 -0800

    IMCEIS-1618 more work on node.rb

--- lib/BundleClosure/lib/node.rb
+++ lib/BundleClosure/lib/node.rb
@@ -14,16 +14,40 @@ class Node
     @children.add(c)
   end
 
+  def add_children(e)
+    @children.merge(e)
+  end
+  
   def delete_child(c)
     @children.delete(c)
+  end
+  
+  def delete_children(e)
+    @children.subtract(e)
+  end
+  
+  def clear_children
+    @children.clear
   end
   
   def add_parent(c)
     @parents.add(c)
   end
 
+  def add_parents(e)
+    @parents.merge(e)
+  end
+  
   def delete_parent(c)
     @parents.delete(c)
+  end
+
+  def delete_parents(e)
+    @parents.subtract(e)
+  end
+
+  def clear_parents
+    @parents.clear
   end
   
   def to_s
--- lib/BundleClosure/test/test_node.rb
+++ lib/BundleClosure/test/test_node.rb
@@ -14,29 +14,49 @@ class TestNode < Test::Unit::TestCase
   def test_children
     na = Node.new(Union.new())
     nb = Node.new(Union.new())
+    nc = Node.new(Union.new())
+    nd = Node.new(Union.new())
 
     assert_empty na.children
-    assert_empty nb.children
     
     na.add_child(nb)
     assert_equal Set.new([nb]), na.children
 
     na.delete_child(nb)
     assert_empty nb.children
+
+    na.add_children([nb, nc, nd])
+    assert_equal Set.new([nb, nc, nd]), na.children
+
+    na.delete_children([nb, nc])
+    assert_equal Set.new([nd]), na.children
+
+    na.clear_children
+    assert_empty na.children
   end
   
   def test_parents
     na = Node.new(Union.new())
     nb = Node.new(Union.new())
+    nc = Node.new(Union.new())
+    nd = Node.new(Union.new())
 
     assert_empty na.parents
-    assert_empty nb.parents
     
     na.add_parent(nb)
     assert_equal Set.new([nb]), na.parents
 
     na.delete_parent(nb)
     assert_empty nb.parents
+
+    na.add_parents([nb, nc, nd])
+    assert_equal Set.new([nb, nc, nd]), na.parents
+
+    na.delete_parents([nb, nc])
+    assert_equal Set.new([nd]), na.parents
+
+    na.clear_parents
+    assert_empty na.parents
   end
   
 end
